866566aa03d1d2f1369a5d0c3fc5fa9ec297f521
sashko-kapillar <kapillarua@gmail.com>
2024-07-22 23:51:04 +0300

    add more new files

---
+++ index.php
@@ -0,0 +1,288 @@
+﻿<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K3ZF74M');</script>
+<!-- End Google Tag Manager -->
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<title>Реклама в интернете | Интернет маркетинг</title>
+<meta name="description" content="Реклама в интернете, интернет маркетинг, контекстная реклама">
+	<meta name="keywords" content="реклама,маркетинг,заказать рекламу,создание рекламы,контекстная,контекстная реклама,Googleреклама">
+<!-- Google fonts -->
+
+
+<!-- font awesome -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<!-- bootstrap -->
+<link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" />
+
+<!-- animate.css -->
+<link rel="stylesheet" href="assets/animate/animate.css" />
+<link rel="stylesheet" href="assets/animate/set.css" />
+
+<!-- gallery -->
+<link rel="stylesheet" href="assets/gallery/blueimp-gallery.min.css">
+
+<!-- favicon -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
+
+
+<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K3ZF74M"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+
+<div class="width=100% height=100% align-left"></div><div class="width=100% height=100% align-left"></div><div class="align-left"></div><div style="position:absolute;left:-3072px;top:0"><a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#108;&#105;&#110;&#105;&#121;&#97;&#111;&#107;&#111;&#110;&#46;&#114;&#117;">&#1086;&#1082;&#1085;&#1072;</a> <!-- div --><!-- div end --> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#112;&#114;&#101;&#109;&#105;&#117;&#109;&#107;&#97;&#100;&#114;&#46;&#114;&#117;">&#1092;&#1086;&#1090;&#1086;&#1075;&#1088;&#1072;&#1092;</a> <!-- div --><!-- div end --> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#117;&#110;&#105;&#115;&#104;&#97;&#98;&#108;&#111;&#110;.&#99;&#111;&#109;">html php</a> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#114;&#105;&#116;&#117;&#97;&#108;&#103;&#97;&#114;&#97;&#110;&#116;&#46;&#114;&#117;">&#1087;&#1072;&#1084;&#1103;&#1090;&#1085;&#1080;&#1082;&#1080;</a> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#116;&#117;&#116;&#108;&#111;&#118;&#101;&#46;&#114;&#117;">&#1079;&#1085;&#1072;&#1082;&#1086;&#1084;&#1089;&#1090;&#1074;&#1072;</a></div><div class="padding valign-image-left"></div><div class="padding  valign-image-right"></div><div class="padding valign-image-center"></div><div class="topbar animated fadeInLeftBig"></div><!-- Header Starts --><div class="navbar-wrapper">
+      <div class="container">
+        <div class="navbar navbar-default navbar-fixed-top" role="navigation" id="top-nav">
+          <div class="container">
+            <div class="navbar-header">
+              <!-- Logo Starts -->
+              <a class="navbar-brand" href="#home"><img src="images/logo2.png" alt="logo"></a>
+              <!-- #Logo Ends -->
+
+
+              <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+
+            </div>
+
+
+            <!-- Nav Starts -->
+            <div class="navbar-collapse  collapse">
+
+              <ul class="nav navbar-nav navbar-right scroll">
+           <li class="active"><a href="#home">ГЛАВНАЯ</a></li>
+                 <li ><a href="#about">РЕКЛАМА</a></li>
+                 <li ><a href="#contact">ЗАКАЗАТЬ</a></li>
+				 <li ><a>+380991292962</a></li>
+              </ul>
+				<a href="ua.htm">ua</a> <a class="active" href="/">ru</a>
+            </div>
+            <!-- #Nav Ends -->
+
+          </div>
+        </div>
+
+      </div>
+    </div>
+<!-- #Header Starts -->
+
+
+
+
+<div id="home">
+<!-- Slider Starts -->
+<div id="myCarousel" class="carousel slide banner-slider animated flipInX" data-ride="carousel">
+      <div class="carousel-inner">
+        <!-- Item 1 -->
+        <div class="item active">
+          <img src="images/back1_1.jpg" alt="banner">
+            <div class="carousel-caption">
+            	<div class="caption-wrapper">
+            		<div class="caption-info">
+            		<img src="images/logobig.png" class="animated bounceInUp" alt="logo">
+              		<h1 class="animated bounceInLeft">реклама в интернете</h1>
+              		<p class="animated bounceInRight">У нас самая прогрессивная команда, которая живет и дышит созданием рекламы.</p>
+              		<div class="scroll animated fadeInUp"> <a href="#contact" class="btn btn-default"><i class="fa fa-paper-plane-o"></i> Запусти свою рекламу</a></div>
+              		</div>
+              	</div>
+            </div>
+        </div>
+        <!-- #Item 1 -->
+
+        <!-- Item 1 -->
+        <div class="item">
+          <img src="images/back2.jpg" alt="banner">
+             <div class="carousel-caption">
+            	<div class="caption-wrapper">
+            		<div class="caption-info">
+                <img src="images/logobig.png" class="animated bounceInUp" alt="logo">
+                  <h1 class="animated bounceInLeft">реклама в интернете</h1>
+                  <p class="animated bounceInRight">У нас самая прогрессивная команда, которая живет и дышит созданием рекламы.</p>
+                  <div class="scroll animated fadeInUp"> <a href="#contact" class="btn btn-default"><i class="fa fa-paper-plane-o"></i> Запусти свою рекламу</a></div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <!-- #Item 1 -->
+
+        <!-- Item 1 -->
+        <div class="item">
+          <img src="images/back3.jpg" alt="banner">
+             <div class="carousel-caption">
+            	<div class="caption-wrapper">
+            		<div class="caption-info">
+                <img src="images/logobig.png" class="animated bounceInUp" alt="logo">
+                  <h1 class="animated bounceInLeft">реклама в интернете</h1>
+                  <p class="animated bounceInRight">У нас самая прогрессивная команда, которая живет и дышит созданием рекламы.</p>
+                  <div class="scroll animated fadeInUp"> <a href="#contact" class="btn btn-default"><i class="fa fa-paper-plane-o"></i> Запусти свою рекламу</a></div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <!-- #Item 1 -->
+
+        <!-- Item 1 -->
+        <div class="item">
+          <img src="images/back4.jpg" alt="banner">
+             <div class="carousel-caption">
+            	<div class="caption-wrapper">
+            		<div class="caption-info">
+                <img src="images/logobig.png" class="animated bounceInUp" alt="logo">
+                  <h1 class="animated bounceInLeft">реклама в интернете</h1>
+                  <p class="animated bounceInRight">У нас самая прогрессивная команда, которая живет и дышит созданием рекламы.</p>
+                  <div class="scroll animated fadeInUp"> <a href="#contact" class="btn btn-default"><i class="fa fa-paper-plane-o"></i> Запусти свою рекламу</a></div>
+                  </div>
+                </div>
+            </div>
+        </div>
+        <!-- #Item 1 -->
+      </div>
+      <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon-chevron-left"><i class="fa fa-angle-left"></i></span></a>
+      <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon-chevron-right"><i class="fa fa-angle-right"></i></span></a>
+    </div>
+<!-- #Slider Ends -->
+</div>
+<!-- Cirlce Starts -->
+<div id="about"  class="container spacer about">
+<h2 class="text-center wowload fadeInUp">Хотите продавать больше??? Закажите рекламу!!!</h2>
+  <div class="row">
+  <div class="col-sm-6 wowload fadeInLeft">
+    <h4><i class="fa fa-paint-brush"></i> Создание & Настройка рекламы</h4>
+    <p>Меня зовут, Александр. Я создаю, настраиваю и запускаю рекламу в интернете. Цель - привести больше клиентов в Ваш бизнес. Есть несколько помощников. Используем лучшие современные инструменты. Следим за новостями и новыми разработками в области интернет маркетинга. Создадим, настроим рекламу для Вашего бизнеса и предоставим поддержку. Хоть нас копируют другие,  мы остаемся доступными для всех. Наша реклама работает на клиента. Убедитесь сами!!!<br> Обойдите своих конкурентов уже сегодня! </p>
+
+
+  </div>
+  <div class="col-sm-6 wowload fadeInRight">
+  <h4><i class="fa fa-code"></i>Реклама </h4>
+  <p>Интернет маркетинг (интернет-реклама) - практика использования всех аспектов традиционного маркетинга в Интернете. Другими словами - электронная комерция. Интенсивно развивающаяся часть рекламной отрасли. Удобное использование и охват огромной аудитории делает рекламу в интернете самой конкурентно-способной из всех существующих. Не удивительно, что на нее постоянно растет спрос.</p>
+  </div>
+  </div>
+</div>
+<!-- #Cirlce Ends -->
+
+    <!-- About Starts -->
+<div class="highlight-info">
+<div class="overlay spacer">
+<div class="container">
+<div class="row text-center  wowload fadeInDownBig">
+    <div class="section__title "><h2>Как мы работаем?</h2></div>
+	<div class="col-sm-3 col-xs-6">
+	<i class="fa fa-smile-o  fa-5x"></i><h4><div class="how__item__img"><img src="images/item.png"></div>
+	<p class="how__item__text">Вы оставляете заявку</p></h4>
+	</div>
+	<div class="col-sm-3 col-xs-6">
+	<i class="fa fa-rocket  fa-5x"></i><h4><div class="how__item__img"><img src="images/item1.png"></div>
+	<p class="how__item__text">Мы с Вами связываемся</p></h4>
+	</div>
+	<div class="col-sm-3 col-xs-6">
+	<i class="fa fa-cloud-download  fa-5x"></i><h4><div class="how__item__img"><img src="images/item2.png"></div>
+	<p class="how__item__text">Обсуждение стратегии</p></h4>
+	</div>
+	<div class="col-sm-3 col-xs-6">
+	<i class="fa fa-map-marker fa-5x"></i><h4><div class="how__item__img"><img src="images/item3.png"></div>
+	<p class="how__item__text">Выполнение заказа</p></h4>
+	</div>
+</div>
+</div>
+</div>
+</div>
+<!-- About Ends -->
+
+<div id="contact" class="spacer">
+<!--Contact Starts-->
+
+
+	<div id="contact" class="spacer">
+		<div class="container contactform center">
+<h2 class="text-center  wowload fadeInUp">Свяжитесь с нами, чтобы начать свой проект</h2>
+	<form class="formUp">
+
+		 <div class="row wowload fadeInLeftBig">
+      <div class="col-sm-6 col-sm-offset-3 col-xs-12">
+
+		<!-- Hidden Required Fields -->
+		<input type="hidden" name="project_name" value="modelin.com.ua">
+		<input type="hidden" name="admin_email" value="kapillar@ukr.net">
+		<input type="hidden" name="form_subject" value="заявка новая">
+		<!-- END Hidden Required Fields -->
+
+		<input type="text" name="Name" placeholder="name..." required><br>
+		<input type="text" name="E-mail" placeholder="E-mail..." required><br>
+		<input type="text" name="Phone" placeholder="phone..." required><br>
+		<button  class="btn btn-primary">НАЧАТЬ</button>
+</div>
+</div>
+	</form>
+	</div>
+</div>
+	</div>
+<!--Contact Ends-->
+
+
+
+<!-- Footer Starts -->
+<div class="footer text-center spacer">
+<p class="wowload flipInX"><a href="https://www.facebook.com/people/%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-%D0%AF%D1%86%D1%83%D0%BD%D0%BE%D0%B2/100004688145960" target="_blank"><i class="fa fa-facebook fa-2x"></i></a>
+</p>
+Copyright <script type='text/javascript'>var mdate = new Date();document.write(mdate.getFullYear());</script> Creative Studio. All rights reserved.
+</div>
+<div style="position:absolute;left:-3072px;top:0"><div class="width=100% height=100% align-left"></div><div class="align-left" width="1"></div><a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#108;&#105;&#110;&#105;&#121;&#97;&#111;&#107;&#111;&#110;&#46;&#114;&#117;">&#1086;&#1082;&#1085;&#1072;</a> <!-- div --><!-- div end --> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#112;&#114;&#101;&#109;&#105;&#117;&#109;&#107;&#97;&#100;&#114;&#46;&#114;&#117;">&#1092;&#1086;&#1090;&#1086;&#1075;&#1088;&#1072;&#1092;</a> <!-- div --><!-- div end --> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#117;&#110;&#105;&#115;&#104;&#97;&#98;&#108;&#111;&#110;.&#99;&#111;&#109;">html php</a> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#114;&#105;&#116;&#117;&#97;&#108;&#103;&#97;&#114;&#97;&#110;&#116;&#46;&#114;&#117;">&#1087;&#1072;&#1084;&#1103;&#1090;&#1085;&#1080;&#1082;&#1080;</a> <a href="&#104;&#116;&#116;&#112;&#58;&#47;&#47;&#116;&#117;&#116;&#108;&#111;&#118;&#101;&#46;&#114;&#117;">&#1079;&#1085;&#1072;&#1082;&#1086;&#1084;&#1089;&#1090;&#1074;&#1072;</a></div>
+<!-- # Footer Ends -->
+
+<a href="#home" class="gototop "><i class="fa fa-angle-up  fa-3x"></i></a>
+
+<!-- The Bootstrap Image Gallery lightbox, should be a child element of the document body -->
+<div id="blueimp-gallery" class="blueimp-gallery blueimp-gallery-controls">
+    <!-- The container for the modal slides -->
+    <div class="slides"></div>
+    <!-- Controls for the borderless lightbox -->
+    <h3 class="title">title</h3>
+    <a class="prev"></a>
+    <a class="next"></a>
+    <a class="close">?</a>
+    <!-- The modal dialog, which will be used to wrap the lightbox content -->
+</div>
+
+
+
+<!-- jquery -->
+<script src="assets/jquery-3.5.1.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+
+<!-- wow script -->
+<script src="assets/wow/wow.min.js"></script>
+
+
+<!-- boostrap -->
+<script src="assets/bootstrap/js/bootstrap.js" type="text/javascript" ></script>
+
+<!-- jquery mobile -->
+<script src="assets/mobile/touchSwipe.min.js"></script>
+<script src="assets/respond/respond.js"></script>
+<!-- gallery -->
+<script src="assets/gallery/jquery.blueimp-gallery.min.js"></script>
+
+<!-- custom script -->
+<script src="assets/script.js"></script>
+
+</body>
+</html>
